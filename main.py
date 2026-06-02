eac25cfe964e20185e23bc34b287dacfae4f7e84
rrss76 <ruper.profe@gmail.com>
2026-06-02 21:29:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def get_app_meta_dict():
             ],
         },
         "ios": {
-            "latest": _env("APP_IOS_LATEST", "3.0.4"),
+            "latest": _env("APP_IOS_LATEST", "3.0.7"),
             "min":    _env("APP_IOS_MIN",    "3.0.4"),
             "force":  _env("APP_IOS_FORCE",  "false").lower() == "true",
             "store_url": _env(
